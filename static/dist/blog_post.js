5f5306dec3063574b9a5fac55e063ae069df878e
Empitrix <empitrix@gmail.com>
2024-03-06 19:27:43 +0100

    Applied font for all the -hljs classes

--- static/dist/blog_post.js
+++ static/dist/blog_post.js
@@ -7,28 +7,36 @@ import rehypeSanitize from 'https://esm.sh/rehype-sanitize@6?bundle';
 import rehypeStringify from 'https://esm.sh/rehype-stringify@10?bundle';
 import rehypeFormat from 'https://esm.sh/rehype-format@5?bundle';
 import rehypeHighlight from 'https://esm.sh/rehype-highlight@6';
-// import remarkPrism from 'https://cdn.skypack.dev/remark-prism';
 
-// const file = await unified()
-// const file = await remark()
-const file = await unified()
+const file = await remark()
 	.use(remarkParse)
 	.use(remarkGfm)
 	.use(remarkRehype)  // CUZED
 	.use(rehypeSanitize)
 	.use(rehypeFormat)
 	.use(rehypeStringify)
-	// .use(remarkPrism)
-	// .use(rehypeHighlight)
-	.data('settings', {fragment: true})
 	.use(rehypeHighlight, {plainText: ['txt', 'text']})
-	// .process(markdownData);
-	// .processSync("# Hi\n\n*Hello*\n**Hello**\n***Hello***\n, world!");
 	.processSync(markdownData);
 
-// console.log(file);
-// document.getElementById('marked').innerHTML = file.value;
-// console.log(String(file))
 document.getElementById('marked').innerHTML = String(file);
-// document.getElementById('marked').innerHTML = file.result;
+
+
+/*
+// using typescript classes
+// import { Marked, Renderer } from '@ts-stack/markdown';
+import { Marked, Renderer } from 'https://cdn.jsdelivr.net/npm/@ts-stack/markdown@1.5.0/+esm';
+
+Marked.setOptions ({
+	renderer: new Renderer,
+	gfm: true,
+	tables: true,
+	breaks: false,
+	pedantic: false,
+	sanitize: false,
+	smartLists: true,
+	// highlight: (code, lang) => lang != null ? hljs.highlight(lang, code).value : null,
+	smartypants: false
+});
+document.getElementById('marked').innerHTML = Marked.parse(markdownData);
+*/
 
